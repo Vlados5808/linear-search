d0056430fd1222f292f18ba78960436f3c9c7dde
Vlados5808 <vvladeslav70@gmail.com>
2025-09-08 09:15:22 +0300

    1.js

--- 1.js
+++ 1.js
@@ -1,11 +1,12 @@
 let arr = [1,2,3,4,5,6,7,8,9,10];
-let target = 11;
-
+let target = 6;
+//поверне 5 тому що пошук починається з 0//
 function linearSearch(arr, target) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === target) return i;
   }
   return -1;
 }
+
 
 console.log(linearSearch(arr, target));
